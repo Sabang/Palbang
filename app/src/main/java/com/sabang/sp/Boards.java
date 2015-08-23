79e2cd0eefae896ed1fc5445aba162026f2c9dd4
Hamliet <hamliet@hanyang.ac.kr>
2015-08-23 17:49:27 +0900

    글쓰기 기능

---
+++ app/src/main/java/com/sabang/sp/Boards.java
@@ -0,0 +1,14 @@
+package com.sabang.sp;
+
+import android.content.Intent;
+
+import java.util.ArrayList;
+
+/**
+ * Created by James on 2015-08-23.
+ */
+public class Boards {
+    ArrayList titles = new ArrayList<>();
+    ArrayList contents = new ArrayList<>();
+
+}
